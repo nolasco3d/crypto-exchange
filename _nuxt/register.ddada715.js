4f2de19c1207c4deee4037406e982aaabe937a1c
André Nolasco <nolasco3d@gmail.com>
2022-11-07 09:42:58 -0300

    462ca1a

---
+++ _nuxt/register.ddada715.js
@@ -0,0 +1,1 @@
+import{m as t,e as o,p as s,q as n,f as c,s as a,o as r,l as _}from"./entry.2e26b5ac.js";const l={},d=c("h1",null,"Register",-1);function i(f,m){const e=a;return r(),o("div",null,[d,s(e,{to:"/"},{default:n(()=>[_("Home")]),_:1})])}const p=t(l,[["render",i]]);export{p as default};
